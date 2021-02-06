9f265f48ff3a0c7a2680e0cfa26e9d9c08a5e84a
Mason Howe <mason.a.howe@gmail.com>
2021-02-06 12:11:14 -0700

    update sql schema tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,16 +4,32 @@ CREATE DATABASE employeesDB;
 
 USE employeesDB;
 
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,    
+    salary INT,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    role_id INT,
+    role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
-CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(50)
-)
+
+
