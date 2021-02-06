edbbb0111feb1714633aa16f06511967a89bd09f
Mason Howe <mason.a.howe@gmail.com>
2021-02-06 15:20:35 -0700

    update env file paths

--- db/database.js
+++ db/database.js
@@ -1,14 +1,18 @@
 const mysql = require("mysql2");
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
+
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_PASS);
+console.log(process.env.DB_USER);
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: process.env.DB_HOST,
   port: 3306,
   // Your MySQL username
-  user: "root",
+  user: process.env.DB_USER,
   // Your MySQL password
-  password: "dbPassword",
-  database: "ice_creamDB",
+  password: process.env.DB_PASS,
+  database: "employeesDB",
 });
 
 connection.connect((err) => {
